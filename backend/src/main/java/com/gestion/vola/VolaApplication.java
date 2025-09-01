96600c8102cbe5b5f967bda64c4829f1ed09cfaa
Randy3227 <randyrobinson3227@gmail.com>
2025-09-01 21:52:03 +0300

    oui

---
+++ backend/src/main/java/com/gestion/vola/VolaApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.vola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gestion/vola/VolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.vola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
